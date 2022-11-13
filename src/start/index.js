92bdad66c2550298c3ae7a2554f631ad8795f923
Артур Бобровских <arturbobrovskih@MacBook-Air-Artur.local>
2022-11-13 18:32:24 +0300

    makeup categories

--- src/start/index.js
+++ src/start/index.js
@@ -8,4 +8,21 @@ export class StartPageComponent extends HTMLElement {
 
     this.innerHTML = template;
   }
+
+
+  connectedCallback() {
+    console.log('connectedCallback', this);
+    this.querySelectorAll('.category').forEach((element) => {
+      element.addEventListener('click', this.onCategoryClick);
+    });
+  }
+
+  disconnectedCallback() {
+    // remove all listeners
+  }
+
+  onCategoryClick(event) {
+    const category = event.target.dataset.category;
+    console.log('click', category);
+  }
 }
