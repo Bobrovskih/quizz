f593ac437c98fd42763138ff6eabd9f951b30cac
Артур Бобровских <arturbobrovskih@MacBook-Air-Artur.local>
2022-11-14 20:53:39 +0300

    add birds data

--- src/results/index.js
+++ src/results/index.js
@@ -2,7 +2,6 @@ import { Component } from '../helpers/component';
 import './index.scss';
 import template from './template.html';
 
-
 export class ResultsPage extends Component {
   constructor() {
     super({ template });
