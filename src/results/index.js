855ef3bef88e5b9395c8157c3453595207f89d34
Артур Бобровских <arturbobrovskih@MacBook-Air-Artur.local>
2022-11-20 19:52:52 +0300

    results page translate

--- src/results/index.js
+++ src/results/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import template from './template.html';
 import resultsImage1 from '../assets/results-img-1.jpeg';
 import { router, ROUTER_PATHS } from '../helpers/router';
+import { translate } from '../translate/service';
 
 export class ResultsPage extends Component {
   constructor() {
@@ -17,15 +18,17 @@ export class ResultsPage extends Component {
     this.$text = this.query('.text');
     this.$repeat = this.query('.repeat');
     this.$image = this.query('.image');
+    this.$buttonText = this.query('#gooey-button .text');
 
     const isMax = this.state.maxScore === this.state.score;
 
     this.$image.src = resultsImage1;
-    this.$score.innerText = `${this.state.totalScore || 0} из ${this.state.maxScore} очков`;
-    this.$text.innerText = isMax ? 'Отличная игра! Вы победили!!!' : 'Игра завершена';
+    this.$score.innerText = `${this.state.totalScore || 0} ${translate.t("из")} ${this.state.maxScore} ${translate.t("очков")}`;
+    this.$text.innerText = isMax ? translate.t("Отличная игра! Вы победили!!!") : translate.t('Игра завершена');
     if (!isMax) {
       this.$repeat.classList.add('show');
     }
+    this.$buttonText.innerText = translate.t("Играть еще раз");
 
     this.$repeat.addEventListener('click', () => {
       router.setPage(ROUTER_PATHS.GAME);
